27e1835afd32f9141cbf12ee792e83bb5a48fda4
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-05-02 18:09:00 +0600

    updated type declaration module

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,10 @@
 declare module "captcha-generator-react" {
-  export function generateCaptcha(options?: any): any;
+  export function generateCaptcha(options?: {
+    length?: number;
+    width?: number;
+    height?: number;
+  }): {
+    captchaText: string;
+    captchaImage: string;
+  };
 }
